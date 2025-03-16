a2e26863af32a32ab42c206b8ba85e2cef8ff134
DARSANDEV <ddarsandev@gmail.com>
2025-03-16 18:18:13 +0530

    palindrome

--- PlayGround/PlayGround/GameFactory.cs
+++ PlayGround/PlayGround/GameFactory.cs
@@ -6,6 +6,7 @@ public static class GameFactory {
     public static Game CreateGame( this GameNames gameName    ){
         return gameName switch {
             GameNames.FindFactorial => new Factorial (),
+            GameNames.Palindrome => new Palindrome(),
             _ => throw new ArgumentException("Invalid game type")
         };
     }
--- PlayGround/PlayGround/GameNames.cs
+++ PlayGround/PlayGround/GameNames.cs
@@ -2,6 +2,5 @@
 public enum GameNames 
 {
     FindFactorial,
-    TowerOfHanoi
-    
+    Palindrome  
 }
