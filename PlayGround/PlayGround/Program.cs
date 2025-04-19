7cfdc91af2267e3717c0ec2144b5922b739006f7
DARSANDEV <ddarsandev@gmail.com>
2025-04-19 22:50:51 +0530

    code cleanup

--- PlayGround/PlayGround/Program.cs
+++ PlayGround/PlayGround/Program.cs
@@ -5,36 +5,35 @@ Console.WriteLine("Welcome");
 bool retry = false;
 do
 {
-    try{
-    var gameNames = Enum.GetValues(typeof(GameNames));
-    
-    Console.WriteLine("************* Choose a Game *******************");
-    foreach (var item in gameNames)
+    try
     {
-        Console.WriteLine($"{(int)item} : {item}");
+        var gameNames = Enum.GetValues(typeof(GameNames));
+
+        Console.WriteLine("************* Choose a Game *******************");
+        foreach (var item in gameNames)
+        {
+            Console.WriteLine($"{(int)item} : {item}");
+        }
+        int chosenGameNum;
+        int.TryParse(Console.ReadLine(), out chosenGameNum);
+        GameNames chosenGame = (GameNames)chosenGameNum;
+        Game game = chosenGame.CreateGame();
+        game.GameDescription();
+        Console.WriteLine($"************* Starts {chosenGame} *******************");
+
+        game.StartGame();
+        Console.WriteLine("--------------- Game completed -------------------");
+
     }
-    int chosenGameNum;
-    int.TryParse(Console.ReadLine(),out chosenGameNum);
-    GameNames chosenGame = (GameNames) chosenGameNum;
-
-    //var sampleObj = new SampleObject();
-    Game game = chosenGame.CreateGame();
-    game.GameDescription();
-    Console.WriteLine($"************* Starts {chosenGame} *******************");
-
-    game.StartGame();
-    Console.WriteLine("--------------- Game completed -------------------");
-     Console.WriteLine(" Press 1 to try again ");
+    catch (Exception ex)
+    {
+        Console.WriteLine("Some error occured : {0}", ex);
     }
-    catch(Exception ex ){
-        Console.WriteLine("Some error occured : {0}",ex);
-         Console.WriteLine(" Do you want to try again : Press 1 ");
-    }
-   
-    var  retryInput= int.Parse(Console.ReadLine());
-    retry = (retryInput ==1) ? true : false;
-    
-} while (retry );
+    Console.WriteLine(" Press 1 to try again ");
+    var retryInput = int.Parse(Console.ReadLine());
+    retry = (retryInput == 1) ? true : false;
+
+} while (retry);
+Console.WriteLine("Thank you for Playing");
 
 
- 
